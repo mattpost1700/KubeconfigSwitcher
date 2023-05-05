12cfe8c98faace92a2fdc26a67987fe39f0d11b0
mattpost1700 <72893809+mattpost1700@users.noreply.github.com>
2023-05-05 00:33:15 -0400

    init

---
+++ src/app.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+def start():
+    print("I started!")
+    sleep(5)
+    print("I still started!")
+    sleep(5)
+    print("I no longer started!")
---
+++ src/file_handler.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DEFAULT_KUBE_DIR = Path.home() / ".kube"
+
+
